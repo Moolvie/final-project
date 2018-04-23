3b127f6fab9f80f5905771fb377ded36bf09bf1e
Najee Minter <n_minter520@yahoo.com>
2018-04-23 19:58:43 -0400

    Search bar, and nav
    .Added a mock nav bar
    .Added a mock search bar for searching songs
    .Added documentation

--- Home.php
+++ Home.php
@@ -4,12 +4,34 @@
 	</head>
 	
 	<body>
-		<header class="header">Header</header>
+		<!--Header Content-->
+		<header class="header">
+			<nav class= "nav">
+				<ul class= "navList">
+					<li><a href="">Home</a></li>
+					<li><a href="">Sign/Log in</a></li>
+					<li><a href="">PlayList</a></li>
+				</ul>
+			</nav>
+		</header>
+		
+		<!--Content Layout-->
 		<div class= "container">	
 				<div class= "col" id= "left">Left</div>
-				<div class = "col" id= "center">Content</div>
+				
+				<div class = "col" id= "center">
+					<form class= "searchForm">
+						<input class = "searchBar" type= text name="Search">
+						<input class = "submit" type="Submit" name= "Search" value="Search">
+					</form>
+					Content
+				</div>
+				
 				<div class= "col" id= "right">Right</div>
 		</div>
+		
+		<!--Footer -->
+		<footer  class="footer">Footer</footer>
 	</body>
-	<footer  class="footer">Footer</footer>
+	
 </html>
