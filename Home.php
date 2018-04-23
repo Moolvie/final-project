b6ac3a1f1d15804eb59b88408c04d8c3327fc6f8
Najee Minter <n_minter520@yahoo.com>
2018-04-23 18:58:12 -0400

    Page Layouts and styles
    This can be our base layout for the web page, where we'll display our music, and such.

---
+++ Home.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="mainStyle.css">
+	</head>
+	
+	<body>
+		<header class="header">Header</header>
+		<div class= "container">	
+				<div class= "col" id= "left">Left</div>
+				<div class = "col" id= "center">Content</div>
+				<div class= "col" id= "right">Right</div>
+		</div>
+	</body>
+	<footer  class="footer">Footer</footer>
+</html>
